469e986c6bad4ef39ec1878b4a9b8cb05a651f65
Inzynierkazik <36386399+Inzynierkazik@users.noreply.github.com>
2019-01-21 18:13:13 +0100

    Update Enemy.h

--- Project1/Enemy.h
+++ Project1/Enemy.h
@@ -1,7 +1,22 @@
-﻿#pragma once
+#pragma once
+
+#include <GL/freeglut.h>
 
 class Enemy
 {
 public:
+    Enemy(float x, float y);
+    ~Enemy();
+    bool live;
+    float h;
+    float w;
+    float x;
+    float y;
+    float z;
+    void drawEnemy();
+    void updateEnemy();
+    void renderEnemy();
+
+
 	
 };
