34ec18b38706bc0e863833a3febfeb189cef085c
Inzynierkazik <36386399+Inzynierkazik@users.noreply.github.com>
2019-01-22 11:27:53 +0100

    Update Enemy.h

--- Project1/Enemy.h
+++ Project1/Enemy.h
@@ -12,11 +12,40 @@ public:
     float w;
     float x;
     float y;
-    float z;
+    float x1;
+    float y1;
+    float deltaX = 0.015f;
+    float deltaY = -0.5f;
     void drawEnemy();
     void updateEnemy();
     void renderEnemy();
 
 
-	
+    bool isLive() const;
+
+    void setLive(bool live);
+
+    float getH() const;
+
+    void setH(float h);
+
+    float getW() const;
+
+    void setW(float w);
+
+    float getX1() const;
+
+    void setX1(float x1);
+
+    float getY1() const;
+
+    void setY1(float y1);
+
+    float getX() const;
+
+    void setX(float x);
+
+    float getY() const;
+
+    void setY(float y);
 };
